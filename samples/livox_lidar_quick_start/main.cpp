638146a9878ff5b8ddd5ac887448001fa6537010
Changhyeon Kim <changhyeon.kim@holiday-robotics.com>
2026-05-18 15:18:06 +0900

    fix: modify somethings to build on ubuntu 24.04

--- samples/livox_lidar_quick_start/main.cpp
+++ samples/livox_lidar_quick_start/main.cpp
@@ -1,227 +1,151 @@
-//
-// The MIT License (MIT)
-//
-// Copyright (c) 2022 Livox. All rights reserved.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
+/*
+How to use?
+- ./livox_lidar_quick
+_start ../../../samples/livox_lidar_quick_start/mid360s_config.json
+./livox_lidar_quick
+_start ../../../samples/livox_lidar_quick_start/mid360_config.json
 
-#include "livox_lidar_def.h"
+config내의 lidar IP를 각 모델 시리얼번호에 맞춰서 설정.
+*/
 #include "livox_lidar_api.h"
+#include "livox_lidar_def.h"
 
 #ifdef _WIN32
 #include <winsock2.h>
 #else
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 #endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <thread>
 #include <chrono>
 #include <iostream>
+#include <mutex>
+#include <set>
+#include <thread>
 
-void PointCloudCallback(uint32_t handle, const uint8_t dev_type, LivoxLidarEthernetPacket* data, void* client_data) {
-  if (data == nullptr) {
-    return;
-  }
-  printf("point cloud handle: %u, data_num: %d, data_type: %d, length: %d, frame_counter: %d\n",
-      handle, data->dot_num, data->data_type, data->length, data->frame_cnt);
+// ====== 설정 영역 ======
+// 바꾸려는 센서의 "현재" IP
+static const char* kTargetCurrentIp = "192.168.1.199";
 
-  if (data->data_type == kLivoxLidarCartesianCoordinateHighData) {
-    LivoxLidarCartesianHighRawPoint *p_point_data = (LivoxLidarCartesianHighRawPoint *)data->data;
-    for (uint32_t i = 0; i < data->dot_num; i++) {
-      //p_point_data[i].x;
-      //p_point_data[i].y;
-      //p_point_data[i].z;
-    }
-  }
-  else if (data->data_type == kLivoxLidarCartesianCoordinateLowData) {
-    LivoxLidarCartesianLowRawPoint *p_point_data = (LivoxLidarCartesianLowRawPoint *)data->data;
-  } else if (data->data_type == kLivoxLidarSphericalCoordinateData) {
-    LivoxLidarSpherPoint* p_point_data = (LivoxLidarSpherPoint *)data->data;
-  }
+// 바꾸려는 "새" IP / 마스크 / 게이트웨이
+static const char* kNewLidarIp = "192.168.10.67";
+static const char* kNewNetmask = "255.255.255.0";
+static const char* kNewGateway = "192.168.10.1";
+// =======================
+
+// 이미 처리한 핸들 추적 (중복 호출 방지)
+static std::set<uint32_t> g_processed_handles;
+static std::mutex g_handles_mutex;
+
+void RebootCallback(livox_status status, uint32_t handle,
+                    LivoxLidarRebootResponse* response, void* client_data) {
+  if (response == nullptr) return;
+  printf("[RebootCallback] handle:0x%08x ret_code:%u\n", handle,
+         response->ret_code);
 }
 
-void ImuDataCallback(uint32_t handle, const uint8_t dev_type,  LivoxLidarEthernetPacket* data, void* client_data) {
-  if (data == nullptr) {
-    return;
-  } 
-  printf("Imu data callback handle:%u, data_num:%u, data_type:%u, length:%u, frame_counter:%u.\n",
-      handle, data->dot_num, data->data_type, data->length, data->frame_cnt);
-}
-
-// void OnLidarSetIpCallback(livox_vehicle_status status, uint32_t handle, uint8_t ret_code, void*) {
-//   if (status == kVehicleStatusSuccess) {
-//     printf("lidar set ip slot: %d, ret_code: %d\n",
-//       slot, ret_code);
-//   } else if (status == kVehicleStatusTimeout) {
-//     printf("lidar set ip number timeout\n");
-//   }
-// }
-
-void EscModeSetCallback(livox_status status, uint32_t handle, LivoxLidarAsyncControlResponse *response, void *client_data) {
-  printf("set lidar esc mode, please power off and on lidar!!!!\n");
-  if (response == nullptr) {
-    return;
-  }
-  printf("EscModeSetCallback, status:%u, handle:%u, ret_code:%u, error_key:%u",
-      status, handle, response->ret_code, response->error_key);
-}
-
-void WorkModeCallback(livox_status status, uint32_t handle,LivoxLidarAsyncControlResponse *response, void *client_data) {
-  if (response == nullptr) {
-    return;
-  }
-  printf("WorkModeCallack, status:%u, handle:%u, ret_code:%u, error_key:%u",
-      status, handle, response->ret_code, response->error_key);
-
-}
-
-void RebootCallback(livox_status status, uint32_t handle, LivoxLidarRebootResponse* response, void* client_data) {
-  if (response == nullptr) {
-    return;
-  }
-  printf("RebootCallback, status:%u, handle:%u, ret_code:%u",
-      status, handle, response->ret_code);
-}
-
-void SetIpInfoCallback(livox_status status, uint32_t handle, LivoxLidarAsyncControlResponse *response, void *client_data) {
-  if (response == nullptr) {
-    return;
-  }
-  printf("LivoxLidarIpInfoCallback, status:%u, handle:%u, ret_code:%u, error_key:%u",
-      status, handle, response->ret_code, response->error_key);
+void SetIpInfoCallback(livox_status status, uint32_t handle,
+                       LivoxLidarAsyncControlResponse* response,
+                       void* client_data) {
+  if (response == nullptr) return;
+  printf(
+      "[SetIpInfoCallback] handle:0x%08x status:%u ret_code:%u error_key:%u\n",
+      handle, status, response->ret_code, response->error_key);
 
   if (response->ret_code == 0 && response->error_key == 0) {
+    printf(">>> IP 변경 성공, 센서 재부팅 명령 전송...\n");
     LivoxLidarRequestReboot(handle, RebootCallback, nullptr);
+  } else {
+    printf(">>> IP 변경 실패!\n");
   }
 }
 
-void QueryInternalInfoCallback(livox_status status, uint32_t handle, 
-    LivoxLidarDiagInternalInfoResponse* response, void* client_data) {
-  if (status != kLivoxLidarStatusSuccess) {
-    printf("Query lidar internal info failed.\n");
-    QueryLivoxLidarInternalInfo(handle, QueryInternalInfoCallback, nullptr);
-    return;
-  }
-
-  if (response == nullptr) {
-    return;
-  }
-
-  uint8_t host_point_ipaddr[4] {0};
-  uint16_t host_point_port = 0;
-  uint16_t lidar_point_port = 0;
-
-  uint8_t host_imu_ipaddr[4] {0};
-  uint16_t host_imu_data_port = 0;
-  uint16_t lidar_imu_data_port = 0;
-
-  uint16_t off = 0;
-  for (uint8_t i = 0; i < response->param_num; ++i) {
-    LivoxLidarKeyValueParam* kv = (LivoxLidarKeyValueParam*)&response->data[off];
-    if (kv->key == kKeyLidarPointDataHostIpCfg) {
-      memcpy(host_point_ipaddr, &(kv->value[0]), sizeof(uint8_t) * 4);
-      memcpy(&(host_point_port), &(kv->value[4]), sizeof(uint16_t));
-      memcpy(&(lidar_point_port), &(kv->value[6]), sizeof(uint16_t));
-    } else if (kv->key == kKeyLidarImuHostIpCfg) {
-      memcpy(host_imu_ipaddr, &(kv->value[0]), sizeof(uint8_t) * 4);
-      memcpy(&(host_imu_data_port), &(kv->value[4]), sizeof(uint16_t));
-      memcpy(&(lidar_imu_data_port), &(kv->value[6]), sizeof(uint16_t));
-    }
-    off += sizeof(uint16_t) * 2;
-    off += kv->length;
-  }
-
-  printf("Host point cloud ip addr:%u.%u.%u.%u, host point cloud port:%u, lidar point cloud port:%u.\n",
-      host_point_ipaddr[0], host_point_ipaddr[1], host_point_ipaddr[2], host_point_ipaddr[3], host_point_port, lidar_point_port);
-
-  printf("Host imu ip addr:%u.%u.%u.%u, host imu port:%u, lidar imu port:%u.\n",
-    host_imu_ipaddr[0], host_imu_ipaddr[1], host_imu_ipaddr[2], host_imu_ipaddr[3], host_imu_data_port, lidar_imu_data_port);
-
-}
-
-void LidarInfoChangeCallback(const uint32_t handle, const LivoxLidarInfo* info, void* client_data) {
+void LidarInfoChangeCallback(const uint32_t handle, const LivoxLidarInfo* info,
+                             void* client_data) {
   if (info == nullptr) {
     printf("lidar info change callback failed, the info is nullptr.\n");
     return;
-  } 
-  printf("LidarInfoChangeCallback Lidar handle: %u SN: %s\n", handle, info->sn);
+  }
 
-  // set lidar esc mode
-  SetLivoxLidarEscMode(handle, kLivoxEscSpeedSlow, EscModeSetCallback, nullptr);
-  
-  // set the work mode to kLivoxLidarNormal, namely start the lidar
-  SetLivoxLidarWorkMode(handle, kLivoxLidarNormal, WorkModeCallback, nullptr);
+  // handle을 IP 문자열로 변환
+  struct in_addr addr;
+  addr.s_addr = handle;
+  const char* current_ip = inet_ntoa(addr);
 
-  QueryLivoxLidarInternalInfo(handle, QueryInternalInfoCallback, nullptr);
+  printf("[LidarInfoChange] handle:0x%08x IP:%s SN:%s\n", handle, current_ip,
+         info->sn);
 
-  // LivoxLidarIpInfo lidar_ip_info;
-  // strcpy(lidar_ip_info.ip_addr, "192.168.1.10");
-  // strcpy(lidar_ip_info.net_mask, "255.255.255.0");
-  // strcpy(lidar_ip_info.gw_addr, "192.168.1.1");
-  // SetLivoxLidarLidarIp(handle, &lidar_ip_info, SetIpInfoCallback, nullptr);
+  // 중복 호출 방지
+  {
+    std::lock_guard<std::mutex> lock(g_handles_mutex);
+    if (g_processed_handles.count(handle)) {
+      printf("  -> 이미 처리한 핸들, skip\n");
+      return;
+    }
+    g_processed_handles.insert(handle);
+  }
+
+  // 타겟 IP가 아니면 무시
+  if (strcmp(current_ip, kTargetCurrentIp) != 0) {
+    printf("  -> 타겟(%s) 아님, skip\n", kTargetCurrentIp);
+    return;
+  }
+
+  printf("  -> 타겟 센서 발견! IP 변경 시작: %s -> %s\n", current_ip,
+         kNewLidarIp);
+
+  LivoxLidarIpInfo lidar_ip_info;
+  memset(&lidar_ip_info, 0, sizeof(lidar_ip_info));
+  strncpy(lidar_ip_info.ip_addr, kNewLidarIp,
+          sizeof(lidar_ip_info.ip_addr) - 1);
+  strncpy(lidar_ip_info.net_mask, kNewNetmask,
+          sizeof(lidar_ip_info.net_mask) - 1);
+  strncpy(lidar_ip_info.gw_addr, kNewGateway,
+          sizeof(lidar_ip_info.gw_addr) - 1);
+
+  SetLivoxLidarIp(handle, &lidar_ip_info, SetIpInfoCallback, nullptr);
 }
 
-void LivoxLidarPushMsgCallback(const uint32_t handle, const uint8_t dev_type, const char* info, void* client_data) {
+void LivoxLidarPushMsgCallback(const uint32_t handle, const uint8_t dev_type,
+                               const char* info, void* client_data) {
   struct in_addr tmp_addr;
-  tmp_addr.s_addr = handle;  
-  std::cout << "handle: " << handle << ", ip: " << inet_ntoa(tmp_addr) << ", push msg info: " << std::endl;
-  std::cout << info << std::endl;
+  tmp_addr.s_addr = handle;
+  std::cout << "[PushMsg] handle:0x" << std::hex << handle
+            << " ip:" << inet_ntoa(tmp_addr) << std::endl;
   return;
 }
 
-int main(int argc, const char *argv[]) {
+int main(int argc, const char* argv[]) {
   if (argc != 2) {
-    printf("Params Invalid, must input config path.\n");
+    printf("Usage: %s <config_path>\n", argv[0]);
     return -1;
   }
   const std::string path = argv[1];
 
-  // REQUIRED, to init Livox SDK2
   if (!LivoxLidarSdkInit(path.c_str())) {
     printf("Livox Init Failed\n");
     LivoxLidarSdkUninit();
     return -1;
   }
-  
-  // REQUIRED, to get point cloud data via 'PointCloudCallback'
-  SetLivoxLidarPointCloudCallBack(PointCloudCallback, nullptr);
-  
-  // OPTIONAL, to get imu data via 'ImuDataCallback'
-  // some lidar types DO NOT contain an imu component
-  SetLivoxLidarImuDataCallback(ImuDataCallback, nullptr);
-  
+
   SetLivoxLidarInfoCallback(LivoxLidarPushMsgCallback, nullptr);
-  
-  // REQUIRED, to get a handle to targeted lidar and set its work mode to NORMAL
   SetLivoxLidarInfoChangeCallback(LidarInfoChangeCallback, nullptr);
 
+  printf("=== IP 변경 도구 ===\n");
+  printf("타겟 현재 IP: %s\n", kTargetCurrentIp);
+  printf("타겟 새 IP:   %s\n", kNewLidarIp);
+  printf("센서 검색 중... 60초 후 종료합니다.\n\n");
+
 #ifdef WIN32
-  Sleep(300000);
+  Sleep(60000);
 #else
-  sleep(300);
+  sleep(60);
 #endif
+
   LivoxLidarSdkUninit();
-  printf("Livox Quick Start Demo End!\n");
+  printf("종료.\n");
   return 0;
 }
--- sdk_core/logger_handler/file_manager.h
+++ sdk_core/logger_handler/file_manager.h
@@ -25,26 +25,29 @@
 #ifndef LIVOX_FILE_MANAGER_
 #define LIVOX_FILE_MANAGER_
 
+#include <cstdint>
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
 namespace livox {
 namespace lidar {
 
 uint64_t GetDirTotalSize(const std::string& dir_name);
 
-bool GetFileNames(const std::string& dir_name, std::multimap<std::string, std::string>& files_name);
+bool GetFileNames(const std::string& dir_name,
+                  std::multimap<std::string, std::string>& files_name);
 bool ChangeHiddenFiles(const std::string& dir_name);
 bool ChangeCurrentFileName(const std::string& dir_name, std::string file_name);
-bool StoreFileName(const char* filename, std::multimap<std::string, std::string>& files_name);
+bool StoreFileName(const char* filename,
+                   std::multimap<std::string, std::string>& files_name);
 bool DeleteHidFiles(const std::string& dir_name);
 
 bool MakeDirecotory(std::string dir);
 
 bool IsDirectoryExits(std::string dir);
 
-} // namespace lidar
+}  // namespace lidar
 }  // namespace livox
 
 #endif  // LIVOX_FILE_MANAGER_
